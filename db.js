d3dc5ed62b48f80b3291f8f1703ad294b58344b3
Tiago Borges <tiagoborges2940@gmail.com>
2025-05-07 04:30:50 -0300

    9

--- db.js
+++ db.js
@@ -6,10 +6,12 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 });
+
+export default pool; // Certifique-se de que o pool está sendo exportado como padrão
