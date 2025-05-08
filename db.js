2617837c58d52bd3978e3a3279d9d1eae8568d20
Tiago Borges <tiagoborges2940@gmail.com>
2025-05-08 00:22:09 -0300

    15

--- db.js
+++ db.js
@@ -6,12 +6,8 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.DATABASE_URL, // Use a variável de ambiente
+  ssl: { rejectUnauthorized: false }, // Necessário para conexões seguras
 });
 
 export default pool; // Certifique-se de que o pool está sendo exportado como padrão
